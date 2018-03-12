8966b0d4d1f6ac377426e4f0463aae091961c399
jingjie <jingjie920701@gmail.com>
2018-03-12 14:38:52 +0800

    log example

---
+++ src/main/java/MyLog.java
@@ -0,0 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyLog {
+
+    private static Logger logger = LoggerFactory.getLogger(MyLog.class);
+
+    public static void main(String[] args) {
+
+        logger.info("this is a info");
+        logger.debug("this is a bug");
+        logger.error("this is a error");
+
+    }
+}
